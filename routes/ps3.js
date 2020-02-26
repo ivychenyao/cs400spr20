448c636bb7d1ae2fbff16e529dd348b2b202d7ae
Ivy Chenyao <ivychenyao@gmail.com>
2020-02-26 15:24:35 -0500

    Add new ps3.js

---
+++ routes/ps3.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.render('getData', {string: 'Hello CS400 World!'});
+});
+
+router.post('/', (req,res,next) => {
+    let returnObj = req.body.string;
+    const post = {str: returnObj, len: returnObj.length};
+    res.render('getData', post);
+});
+
+module.exports = router;
