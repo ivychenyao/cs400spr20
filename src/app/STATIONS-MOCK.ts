46f5d35b8734dc9a79eb2232eccb25ffc3f7d1b1
Ivy Chenyao <ivychenyao@gmail.com>
2020-04-25 16:19:39 -0400

    Add additional files

---
+++ src/app/STATIONS-MOCK.ts
@@ -0,0 +1,34 @@
+import {STATION} from './STATION';
+
+export const STATIONS: STATION[] = [
+  {
+    name: 'Alewife',
+    latitude: '-71.194994',
+    longitude: '42.316115',
+    municipality: 'Cambridge',
+  },
+  {
+    name: 'Trapelo Rd @ Porter Rd',
+    latitude: '-71.206707',
+    longitude: '42.395337',
+    municipality: 'Waltham',
+  },
+  {
+    name: 'Forest St @ Harrington Rd',
+    latitude: '-71.217218',
+    longitude: '42.396793',
+    municipality: 'Waltham',
+  },
+  {
+    name: 'Waverly Oaks',
+    latitude: '-71.198023',
+    longitude: '42.390359',
+    municipality: 'Waltham',
+  },
+  {
+    name: 'Forest St @ Beaver St',
+    latitude: '-71.225949',
+    longitude: '42.385843',
+    municipality: 'Waltham',
+  },
+]
